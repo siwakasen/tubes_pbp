b7f180c511f20bee7908690fa552b7815413cdab
siwakasen <maderiksi20@gmail.com>
2023-11-04 12:30:25 +0700

    add dummy on map

--- lib/view/maps/map.dart
+++ lib/view/maps/map.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:geocoding/geocoding.dart';
 import 'package:latlong2/latlong.dart';
 
 class OpenMap extends StatefulWidget {
@@ -11,14 +10,43 @@ class OpenMap extends StatefulWidget {
 }
 
 class _OpenMapState extends State<OpenMap> {
-  late Position _currentPosition;
+  // late Position _currentPosition;
+  Position _currentPosition = Position(
+      longitude: -122.08395287867832,
+      latitude: 37.42342342342342,
+      timestamp: DateTime.now(),
+      accuracy: 2000.0,
+      altitude: 0.5,
+      altitudeAccuracy: 0.0,
+      heading: 30.0,
+      headingAccuracy: 0.0,
+      speed: -122.08395287867832,
+      speedAccuracy: 0.5);
+  @override
+  void initState() {
+    _getCurrentLocation();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    print(_currentPosition.longitude);
+    print(_currentPosition.latitude);
+    print(_currentPosition.timestamp);
+    print(_currentPosition.accuracy);
+    print(_currentPosition.altitudeAccuracy);
+    print(_currentPosition.heading);
+    print(_currentPosition.headingAccuracy);
+    print(_currentPosition.speed);
+    print(_currentPosition.longitude);
+    print(_currentPosition.speedAccuracy);
+    print("HEREEEEEEEEEEEEEEEEEEEEEEEEEE");
     return Stack(
       children: [
         FlutterMap(
             options: MapOptions(
-                initialCenter: LatLng(-12.069783, -77.034057),
+                initialCenter: LatLng(
+                    _currentPosition.latitude, _currentPosition.longitude),
                 initialZoom: 13.0),
             children: [
               TileLayer(
